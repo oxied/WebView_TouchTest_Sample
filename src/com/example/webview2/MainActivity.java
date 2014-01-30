9ac910df0f65d31d95b12116281452a533923203
Gleb Mozheiko <gmozheiko@ruswizards.com>
2014-01-30 17:40:52 +0400

    Added change visibility button.

--- src/com/example/webview2/MainActivity.java
+++ src/com/example/webview2/MainActivity.java
@@ -3,17 +3,28 @@ package com.example.webview2;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
+
+	private WebView webView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		WebView myWebView = (WebView) findViewById(R.id.webview);
-		myWebView.loadUrl("http://www.google.ru/");
+		webView = (WebView) findViewById(R.id.webview);
+		webView.loadUrl("http://bash.im/");
+		webView.setWebViewClient(new WebViewClient() {
+			@Override
+			public boolean shouldOverrideUrlLoading(WebView view, String url) {
+				view.loadUrl(url);
+				return true;
+			}
+		});
 	}
 
 	@Override
@@ -21,6 +32,15 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	public void onShowWebviewClicked(View view) {
+		if (webView.getVisibility() == View.VISIBLE) {
+			webView.setVisibility(View.GONE);
+		} else {
+			webView.setVisibility(View.VISIBLE);
+			webView.loadUrl("http://bash.im/");
+		}
 	}
 
 }
